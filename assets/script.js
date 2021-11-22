89be9fd74527578310b0cbe2fae01613d0b781a4
mlbarre <mlbarrera1538@gmail.com>
2021-11-21 22:08:00 -0600

    added script

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,33 @@
 // Assignment code here
+var generatePassword =  function() {
+    var specialChara = window.prompt("Please enter one of the following special characters: @ # $ !");
+
+    if (specialChara === "@" || specialChara === "#" || specialChara === "$" || specialChara === "!") {
+        window.alert("You've picked a special character!")
+    } else {
+        window.alert("You will have to pick one to continue.")
+    }
+
+    var passLength = window.prompt("Please choose how many characters: 8 - 128");
+        if (passLength >= 8 && passLength <= 128) {
+        passLength = 8 === 128;
+        console.log(passLength); 
+    } else (passLength < 8 || passLength > 128); {
+        window.alert("Please choose a valid number between 8 and 128.");
+    }
+        var specialCase = window.prompt("Please choose one of the following: 'lowercase', 'uppercase', or 'numeric'.")
+            if (specialCase === "lowercase") {
+                window.prompt("You chosen lowercase. Correct?");
+                specialCase = true;
+            } else if (specialCase === "uppercase") {
+                window.prompt("You've chosen uppercase. Correct?");
+                specialCase = true;
+            } else {
+                window.prompt("You've chosen numeric. Correct?");
+                specialCase = true;
+            }
+    
+}
 
 
 // Get references to the #generate element
@@ -12,10 +41,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-var getRandomPass =  function() {
-    if (generateBtn.onclick) {
-        window.prompt("Please selected the following characters.");
-
-}
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generatePassword();
